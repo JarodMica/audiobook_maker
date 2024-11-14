c9dad4aa92db7f6ba4a5f0001d73e9c1aa76b762
Jarod Mica <jarodmica@gmail.com>
2024-11-14 02:56:30 -0800

    fix

--- finish_styletts_install.bat
+++ finish_styletts_install.bat
@@ -1,9 +1,8 @@
-
-if exist "runtime" (
-    set PATH=%~dp0portable_git\bin;%PATH%
+@echo off
+if exist "runtime\" (
     runtime\python.exe -m pip install nltk
     runtime\python.exe .\modules\styletts-api\modules\StyleTTS2\styletts2\download_punkt.py
-    git clone https://github.com/JarodMica/eSpeak-Files.git
+    portable_git\bin\git.exe clone https://github.com/JarodMica/eSpeak-Files.git
     xcopy "eSpeak-Files\eSpeak NG" "eSpeak NG" /E /I /H /Y
     rmdir /S /Q "eSpeak-Files"
 ) else (
